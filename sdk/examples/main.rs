c875ee800f55029e7926295fc9de11428431ab3d
Brord van Wierst <brord@brord.net>
2024-05-22 16:22:07 +0200

    clippy and fmt

--- sdk/examples/main.rs
+++ sdk/examples/main.rs
@@ -1,39 +1,35 @@
 // Import reexports from the Demia SDK
 use demia_sdk::{
-    identity::iota::{
-        did::DID,
-        iota::{IotaDID, NetworkName},
-    },
+    identity::iota::iota::{IotaDID, NetworkName},
     iota_client::Client as IotaClient,
     iota_stronghold::Stronghold,
     streams::{transport::utangle::Client as StreamsClient, TransportMessage},
 };
-use dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = dotenv::var("URL")?;
 
     // Initialize IOTA client
-    let iota_client = IotaClient::builder().with_node(&url)?.finish()?;
+    let _iota_client = IotaClient::builder().with_node(&url)?.finish()?;
 
     // Example: Interact with the IOTA module
     // ...
 
     // Initialize Stronghold for secure storage
-    let stronghold = Stronghold::default();
+    let _stronghold = Stronghold::default();
 
     // Example: Use Stronghold for secure storage
     // ...
 
     // Example: Interact with the Identity module
-    let mut iota_identity = IotaDID::from_alias_id("seed_for_identity", &NetworkName::try_from("smr").unwrap());
+    let iota_identity = IotaDID::from_alias_id("seed_for_identity", &NetworkName::try_from("smr").unwrap());
 
     // Print the created DID
     println!("Created DID: {}", iota_identity);
 
     // Example: Interact with Streams module
-    let mut client = StreamsClient::<TransportMessage>::new("mysql://user:password@localhost/db");
+    let _client = StreamsClient::<TransportMessage>::new("mysql://user:password@localhost/db");
 
     Ok(())
 }
